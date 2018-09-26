86575cf4d3fa190146d40cb56b90a7e0513fa884
FredJiang <jiang.fei@ffcg.se>
2018-09-26 14:50:05 +0800

    task list get use Subject

--- TaskManage/src/app/_services/tasks.service.ts
+++ TaskManage/src/app/_services/tasks.service.ts
@@ -3,35 +3,44 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TaskLine } from '../_models/taskLine';
 import { NewTaskCommand } from '../_commands/newTask.command';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { Task } from '../_models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
-  tasksSubject = new Subject<TaskLine[]>();
+  tasksSubject = new BehaviorSubject<TaskLine[]>([]);
   tasks$ = this.tasksSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
   addLine(title: string) {
-    return this.http.post<TaskLine>(`${environment.baseUrl}/tasklines`, { title: title})
-    .pipe(
-      tap(_ => this.getAll().subscribe())
-    );
+    return this.http.post<TaskLine>(`${environment.baseUrl}/tasklines`, { title: title })
+      .pipe(
+        tap(line => {
+          const currentValue = this.tasksSubject.value;
+          const updatedValue = [...currentValue, line];
+          this.tasksSubject.next(updatedValue);
+        })
+      );
   }
 
   addTask(task: NewTaskCommand) {
-    return this.http.post<TaskLine>(`${environment.baseUrl}/tasks`, task).pipe(
-      tap(_ => this.getAll().subscribe())
+    return this.http.post<Task>(`${environment.baseUrl}/tasks`, task).pipe(
+      tap(t => {
+        const currentLine = this.tasksSubject.value.find(x => x.id === task.lineId);
+        const newTasks = [...currentLine.tasks, t];
+        currentLine.tasks = newTasks;
+      })
     );
   }
 
   getAll() {
     return this.http.get<TaskLine[]>(`${environment.baseUrl}/tasklines`)
-    .pipe(
-      tap(lines => this.tasksSubject.next(lines))
-    );
+      .pipe(
+        tap(lines => this.tasksSubject.next(lines))
+      );
   }
 }
