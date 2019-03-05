b3dc0320cb0cd71ae40c3768cb2bc245a312ee86
FredJiang <jiangf@shinetechchina.com>
2019-03-05 15:13:57 +0800

    ViewEncapsulation

--- TaskManage/src/app/basic/component-style/component-style.component.ts
+++ TaskManage/src/app/basic/component-style/component-style.component.ts
@@ -4,7 +4,11 @@ import { Hero } from './hero';
 @Component({
   selector: 'app-component-style',
   templateUrl: './component-style.component.html',
-  styleUrls: ['./component-style.component.css']
+  styleUrls: ['./component-style.component.css'],
+
+  // encapsulation: ViewEncapsulation.Emulated  默认，只进不出，全局样式能进来，组件样式出不去
+  // encapsulation: ViewEncapsulation.ShadowDom 不进不出，没有样式能进来，组件样式出不去
+  // encapsulation: ViewEncapsulation.None 能进能出
 })
 export class ComponentStyleComponent {
   hero = new Hero(
